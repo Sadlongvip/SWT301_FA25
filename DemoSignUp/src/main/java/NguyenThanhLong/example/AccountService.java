df611d73b791df28d960206dcea1426dbf8c71ec
Sadlongvip <sadlongvip@gmail.com>
2025-10-08 17:04:58 +0700

    Add files via upload

---
+++ DemoSignUp/src/main/java/NguyenThanhLong/example/AccountService.java
@@ -0,0 +1,32 @@
+package NguyenThanhLong.example;
+
+import java.util.regex.Pattern;
+
+public class AccountService {
+    public boolean registerAccount(String username,String password,String email){
+        if (username == null || username.trim().isEmpty()) {
+            System.out.println("Username is null or empty");
+            return false;
+        }
+        // password require more 6 char
+        if (password == null || password.length() < 6) {
+            System.out.println("Password is less than 6 characters");
+            return false;
+        }
+        if (email == null || email.trim().isEmpty()) {
+            System.out.println("Email is null or empty");
+            return false;
+        }
+        if(!isValidEmail(email)){
+            System.out.println("Email wrong format");
+            return false;
+        }
+
+        System.out.println("Successfully registered account");
+        return true;
+    }
+    private static boolean isValidEmail(String email){
+        Pattern EMAIL_PATTERN = Pattern.compile("^[^@\\s]+@[^@\\s]+\\.[^@\\s]+$");
+        return email != null && EMAIL_PATTERN.matcher(email).matches();
+    }
+}
